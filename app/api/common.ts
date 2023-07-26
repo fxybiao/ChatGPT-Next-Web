4a1ab7252e23d028d77b71346c758e0105d8ecbc
fxybiao <140057493+fxybiao@users.noreply.github.com>
2023-07-26 19:30:30 +0800

    Update common.ts

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { ChatGPTUnofficialProxyAPI } from 'chatgpt'
 
 export const OPENAI_URL = "api.openai.com";
 const DEFAULT_PROTOCOL = "https";
@@ -7,86 +8,95 @@ const BASE_URL = process.env.BASE_URL || OPENAI_URL;
 const DISABLE_GPT4 = !!process.env.DISABLE_GPT4;
 
 export async function requestOpenai(req: NextRequest) {
-  const controller = new AbortController();
-  const authValue = req.headers.get("Authorization") ?? "";
-  const openaiPath = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll(
-    "/api/openai/",
-    "",
-  );
+    const api = new ChatGPTUnofficialProxyAPI({
+    accessToken: process.env.OPENAI_ACCESS_TOKEN
+  })
+    const res = await api.sendMessage('Hello World!')
+    console.log(res.text)
 
-  let baseUrl = BASE_URL;
+  const res = await api.sendMessage('Hello World!')
+  console.log(res.text)
+  return res
+  // const controller = new AbortController();
+  // const authValue = req.headers.get("Authorization") ?? "";
+  // const openaiPath = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll(
+  //   "/api/openai/",
+  //   "",
+  // );
 
-  if (!baseUrl.startsWith("http")) {
-    baseUrl = `${PROTOCOL}://${baseUrl}`;
-  }
+  // let baseUrl = BASE_URL;
 
-  console.log("[Proxy] ", openaiPath);
-  console.log("[Base Url]", baseUrl);
+  // if (!baseUrl.startsWith("http")) {
+  //   baseUrl = `${PROTOCOL}://${baseUrl}`;
+  // }
 
-  if (process.env.OPENAI_ORG_ID) {
-    console.log("[Org ID]", process.env.OPENAI_ORG_ID);
-  }
+  // console.log("[Proxy] ", openaiPath);
+  // console.log("[Base Url]", baseUrl);
 
-  const timeoutId = setTimeout(() => {
-    controller.abort();
-  }, 10 * 60 * 1000);
+  // if (process.env.OPENAI_ORG_ID) {
+  //   console.log("[Org ID]", process.env.OPENAI_ORG_ID);
+  // }
 
-  const fetchUrl = `${baseUrl}/${openaiPath}`;
-  const fetchOptions: RequestInit = {
-    headers: {
-      "Content-Type": "application/json",
-      "Cache-Control": "no-store",
-      Authorization: authValue,
-      ...(process.env.OPENAI_ORG_ID && {
-        "OpenAI-Organization": process.env.OPENAI_ORG_ID,
-      }),
-    },
-    method: req.method,
-    body: req.body,
-    // @ts-ignore
-    duplex: "half",
-    signal: controller.signal,
-  };
+  // const timeoutId = setTimeout(() => {
+  //   controller.abort();
+  // }, 10 * 60 * 1000);
 
-  // #1815 try to refuse gpt4 request
-  if (DISABLE_GPT4 && req.body) {
-    try {
-      const clonedBody = await req.text();
-      fetchOptions.body = clonedBody;
+  // const fetchUrl = `${baseUrl}/${openaiPath}`;
+  // const fetchOptions: RequestInit = {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Cache-Control": "no-store",
+  //     Authorization: authValue,
+  //     ...(process.env.OPENAI_ORG_ID && {
+  //       "OpenAI-Organization": process.env.OPENAI_ORG_ID,
+  //     }),
+  //   },
+  //   method: req.method,
+  //   body: req.body,
+  //   // @ts-ignore
+  //   duplex: "half",
+  //   signal: controller.signal,
+  // };
 
-      const jsonBody = JSON.parse(clonedBody);
+  // // #1815 try to refuse gpt4 request
+  // if (DISABLE_GPT4 && req.body) {
+  //   try {
+  //     const clonedBody = await req.text();
+  //     fetchOptions.body = clonedBody;
 
-      if ((jsonBody?.model ?? "").includes("gpt-4")) {
-        return NextResponse.json(
-          {
-            error: true,
-            message: "you are not allowed to use gpt-4 model",
-          },
-          {
-            status: 403,
-          },
-        );
-      }
-    } catch (e) {
-      console.error("[OpenAI] gpt4 filter", e);
-    }
-  }
+  //     const jsonBody = JSON.parse(clonedBody);
 
-  try {
-    const res = await fetch(fetchUrl, fetchOptions);
+  //     if ((jsonBody?.model ?? "").includes("gpt-4")) {
+  //       return NextResponse.json(
+  //         {
+  //           error: true,
+  //           message: "you are not allowed to use gpt-4 model",
+  //         },
+  //         {
+  //           status: 403,
+  //         },
+  //       );
+  //     }
+  //   } catch (e) {
+  //     console.error("[OpenAI] gpt4 filter", e);
+  //   }
+  // }
 
-    // to prevent browser prompt for credentials
-    const newHeaders = new Headers(res.headers);
-    newHeaders.delete("www-authenticate");
-    // to disable nginx buffering
-    newHeaders.set("X-Accel-Buffering", "no");
+  // try {
+  //   const res = await fetch(fetchUrl, fetchOptions);
 
-    return new Response(res.body, {
-      status: res.status,
-      statusText: res.statusText,
-      headers: newHeaders,
-    });
-  } finally {
-    clearTimeout(timeoutId);
-  }
+  //   // to prevent browser prompt for credentials
+  //   const newHeaders = new Headers(res.headers);
+  //   newHeaders.delete("www-authenticate");
+  //   // to disable nginx buffering
+  //   newHeaders.set("X-Accel-Buffering", "no");
+
+  //   return new Response(res.body, {
+  //     status: res.status,
+  //     statusText: res.statusText,
+  //     headers: newHeaders,
+  //   });
+  // } finally {
+  //   clearTimeout(timeoutId);
+  // }
 }
